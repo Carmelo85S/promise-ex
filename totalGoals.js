2796219c0d4b2dabdfe222a10654b7908af9fc05
Carmelo85S <carmelo.salis85@gmail.com>
2025-03-05 12:37:21 +0100

    random advice based of total goal in the game

--- totalGoals.js
+++ totalGoals.js
@@ -1,6 +1,7 @@
-const totalGoals = () => {
+let result = Math.floor(Math.random() * 10 + 1);
+
+const totalGoals = (result) => {
     return new Promise((resolve, reject) => {
-        let result = Math.floor(Math.random() * 10 + 1);
         
         if (result > 5) {
             resolve(`Yoo man, what a game! ${result} goals were scored.`);
@@ -10,10 +11,32 @@ const totalGoals = () => {
     });
 };
 
-totalGoals()
+totalGoals(result)
     .then((message) => {
         console.log(message)
     })
     .catch((error) => {
         console.error("Boring...", error)
     });
+
+
+const fetchAdviceById = (result) => {
+    //logic
+    fetch(`https://api.adviceslip.com/advice/${result}`)
+    .then((response)=> {
+        if(!response.ok){
+            throw new Error("Network response not ok")
+        }
+        return response.json();
+    })
+    .then(data => {
+        const advice = data.slip.advice;
+        console.log(`Advice (ID: ${result}): ${advice}`)
+    })
+    .catch((error) => {
+        console.error("Error fetching advice", error)
+    });
+}
+
+fetchAdviceById(result)
+
