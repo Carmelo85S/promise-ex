56777b8acbe0e3c1df7e1349cb391db433523bf7
Carmelo85S <carmelo.salis85@gmail.com>
2025-03-10 10:24:09 +0100

    turned promises into async/await in node.js, fetch data from api with Async/Await, combine the promises ad fetch

--- totalGoals.js
+++ totalGoals.js
@@ -1,42 +1,75 @@
-let result = Math.floor(Math.random() * 10 + 1);
+let goalsScored = Math.floor(Math.random() * 10 + 1);
 
-const totalGoals = (result) => {
+const totalGoals = (goalsScored) => {
     return new Promise((resolve, reject) => {
         
-        if (result > 5) {
-            resolve(`Yoo man, what a game! ${result} goals were scored.`);
+        if (goalsScored > 5) {
+            resolve(`Yoo man, what a game! ${goalsScored} goals were scored.`);
         } else {
-            reject(`Only ${result} goals. Not so exciting`);
+            reject(`Only ${goalsScored} goals. Not so exciting`);
         }
     });
 };
 
-totalGoals(result)
-    .then((message) => {
-        console.log(message)
-    })
-    .catch((error) => {
+
+
+//With promises
+
+// totalGoals(goalsScored)
+//     .then((message) => {
+//         console.log(message)
+//     })
+//     .catch((error) => {
+//         console.error("Boring...", error)
+//     });
+
+
+// const fetchAdviceById = (goalsScored) => {
+//     //logic
+//     fetch(`https://api.adviceslip.com/advice/${result}`)
+//     .then((response)=> {
+//         if(!response.ok){
+//             throw new Error("Network response not ok")
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         const advice = data.slip.advice;
+//         console.log(`Advice (ID: ${goalsScored}): ${advice}`)
+//     })
+//     .catch((error) => {
+//         console.error("Error fetching advice", error)
+//     });
+// }
+
+// fetchAdviceById(goalsScored)
+
+
+//with Async await
+const tot = async() => {
+    try {
+        const value = await totalGoals(goalsScored);
+        console.log(value)
+
+    } catch (error) {
         console.error("Boring...", error)
-    });
+    }
+}
+tot(goalsScored)
 
-
-const fetchAdviceById = (result) => {
-    //logic
-    fetch(`https://api.adviceslip.com/advice/${result}`)
-    .then((response)=> {
+const fetchAdviceById = async() => {
+    try {
+        const response = await fetch(`https://api.adviceslip.com/advice/${goalsScored}`)
         if(!response.ok){
             throw new Error("Network response not ok")
         }
-        return response.json();
-    })
-    .then(data => {
+        const data = await response.json();
+        console.log(data)
         const advice = data.slip.advice;
-        console.log(`Advice (ID: ${result}): ${advice}`)
-    })
-    .catch((error) => {
-        console.error("Error fetching advice", error)
-    });
+        console.log(`Advice (ID: ${goalsScored}): ${advice}`)
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-fetchAdviceById(result)
-
+fetchAdviceById(goalsScored)
